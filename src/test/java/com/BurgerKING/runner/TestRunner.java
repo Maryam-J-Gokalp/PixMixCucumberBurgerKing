c87263d2f17b7a3cebb96649f86c4221baafffb6
Yasin <yasin.deger48@gmail.com>
2022-06-28 21:23:02 +0300

    last version adapted for jenkins

--- src/test/java/com/BurgerKING/runner/TestRunner.java
+++ src/test/java/com/BurgerKING/runner/TestRunner.java
@@ -15,7 +15,8 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/BurgerKING/stepDefinitions",
         dryRun = true,
-        tags = ""
+        tags = "",
+        publish = true
 
 )
 
